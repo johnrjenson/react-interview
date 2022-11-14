fc58974d6ce97ce69baee007658aafd64157a51d
John Jenson <jjenson@gmail.com>
2022-11-14 15:49:12 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css'
+
+function App() {
+  //TODO Make this component load the data from the launch API and display the mission names
+  return <div/>
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    // https://vitest.dev/config/#reporters
+    reporters: 'verbose',
+
+    // https://vitest.dev/config/#environment
+    environment: 'jsdom',
+
+    /**
+     * https://vitest.dev/config/#setupfiles
+     * They will be run before each test file.
+     */
+    setupFiles: ['./src/setupTests.js']
+
+  }
+})
